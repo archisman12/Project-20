77a9b1fc12f42d39fca51c4c10ea22abf6fc6703
archisman12 <75253966+archisman12@users.noreply.github.com>
2021-01-08 22:41:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var PLAY=1;var END=0
+ gameState=PLAY;
+
+var backGround,backGroundImage
+var catImage,cat;
+var mouseImage,mouse;
+
+function preload() {
+    //load the images here
+    backGroundImage=loadImage("garden.png")
+    catImage=loadAnimation("tomTwo.png","tomThree.png")
+    mouseImage=loadAnimation("jerryTwo.png","jerryThree.png")
+    catOneImage=loadImage("tomOne.png")
+    mouseOneImage=loadImage("jerryOne.png")
+    catFourImage=loadImage("tomFour.png")
+    mouseFourImage=loadImage("jerryFour.png")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    backGround=createSprite(500,400)
+    backGround.addImage(backGroundImage)
+
+    mouse=createSprite(215,650)
+    mouse.addImage(mouseOneImage)
+    mouse.scale=0.1;
+   // mouse.setCollider("rectangle",0,0,0,0)
+  
+    
+
+    cat=createSprite(720,620)
+    cat.addImage(catOneImage)
+    cat.scale=0.2;
+   // cat.setCollider("rectangle",0,0,0,0)
+    
+
+    
+   
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    if(gameState===PLAY){
+   keypressed()
+    if(cat.x-mouse.x<(cat.width-mouse.width)){
+       gameState=END
+    
+    }
+
+    
+    }
+
+    else if(gameState===END){
+        cat.velocityX=-0;
+        cat.addAnimation("catStanding",catFourImage)
+        cat.changeAnimation("catStanding")
+        mouse.addAnimation("mouseChasing",mouseFourImage)
+        mouse.changeAnimation("mouseChasing")
+
+    }
+   drawSprites();
+}
+
+
+function keypressed(){
+
+  //For moving and changing animation write code here
+if(keyCode===(LEFT_ARROW)){
+cat.velocityX=-5;
+cat.addAnimation("catRunning",catImage);
+cat.changeAnimation("catRunning");
+mouse.addAnimation("mouseStanding",mouseImage);
+mouse.changeAnimation("mouseStanding");
+
+}
+
+}
